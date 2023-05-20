047510bdc8637ca5d142e5199079c0aecb28074e
Mikhail <mikhail3468@yandex.ru>
2023-05-20 09:42:51 +0300

    feat: add CardDeal component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -7,7 +7,7 @@ type Props = {
 const Button: FC<Props> = ({ styles }) => (
   <button
     type="button"
-    className={`rounded-xl py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none ${styles}`}
+    className={`hover:shadow-green rounded-[10px] py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none ${styles}`}
   >
     Get Started
   </button>
